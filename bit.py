7a9a1468f33a7fe0267db6bd290cfcf827e82f08
lxf310 <dawn_phoenix@126.com>
2019-04-19 20:49:25 +0800

    Single Number

--- bit.py
+++ bit.py
@@ -29,3 +29,19 @@ class Solution(object):
         # 整数的负数相当于改正数二进制位各位取反，之后再加一；
         # 所以n & -n相当于截取n的最低位1的位置
         return (n>0) and n&-n == n
+
+
+
+"""
+https://leetcode-cn.com/problems/single-number/
+"""
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ret = nums[0]
+        for i in range(1, len(nums)):
+            ret ^= nums[i]
+        return ret
